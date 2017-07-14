07bac0530bd6cf4dc949ff7dcab810161667a009
Sergii Kamenskyi <sergii.kamenskyi@zalando.de>
2017-07-14 12:51:54 +0200

    ARUHA-654: Update API documentation in nakadi-manual. cleanup

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-npm install
 
+DIR="${HOME}/.gitbook/versions/3.2.2"
+if [ -d "${DIR}" ]; then
+    echo "Found ${DIR}"
+    exit 0;
+fi
 
 #HACK: ignore error go to directory and fix it there
 #https://github.com/GitbookIO/gitbook/issues/1818
+
+echo "Not found ${DIR}. Installing";
 set -e
 gitbook fetch 3.2.2
 set +e
-cd ~/.gitbook/versions/3.2.2
+cd ${DIR}
 npm install
 cd -
